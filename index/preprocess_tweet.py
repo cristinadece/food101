c7c669bead5d175d95454bc4ae3cbd6f6f83ad70
Cristina Muntean <cristina.i.muntean@gmail.com>
2017-05-15 15:04:31 +0200

    minor change to location
    Former-commit-id: e54bdc3bfd7591f6a7da98741b54aa3a4b4d791f

---
+++ index/preprocess_tweet.py
@@ -0,0 +1,5 @@
+"""
+
+"""
+# food101 : preprocess_tweet.py
+# Created by muntean on 5/15/17
--- location/get_location_from_tweet.py
+++ location/get_location_from_tweet.py
@@ -181,7 +181,9 @@ def getLocationData(tweet):
         tweet_place_country = None
         tweet_place_country_code = None
 
-    return tweet_coords, tweet_place_city, tweet_place_country, tweet_place_country_code
+    user_location = tweet['user']['location']
+
+    return tweet_coords, tweet_place_city, tweet_place_country, tweet_place_country_code, user_location
 
 
 if __name__ == '__main__':
