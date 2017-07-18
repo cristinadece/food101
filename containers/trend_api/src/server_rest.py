5ce6c50de34711c2c11d4c1bc50af602edd70f91
Vinícius Monteiro <vinicezarml@gmail.com>
2017-07-18 14:37:28 +0200

    trending api
    Former-commit-id: f9687219db332b02cd3d41a157d28b365991b2d2

---
+++ containers/trend_api/src/server_rest.py
@@ -0,0 +1,61 @@
+import json
+from flask import Flask, request
+from flask_restful import Resource, Api
+from es_trend_queries import get_categories_trends_filtered_by_country, get_countries_trends_filtered_by_category
+from carto_db_sync import sync_db_view
+from flask_cors import CORS
+
+app = Flask(__name__)
+api = Api(app)
+CORS(app)
+
+class Status (Resource):
+    def get(self):
+        return {
+            'name': 'Trends'
+            , 'status': 'on'
+        }
+
+class CountriesTrends(Resource):
+
+    def post(self):
+        category = request.form['category']
+        dateBegin = request.form['dateBegin']
+        dateEnd = request.form['dateEnd']
+        analysis_type = request.form['analysis_type']
+        result = {"results": get_countries_trends_filtered_by_category(category, dateBegin, dateEnd, analysis_type)}
+        result = json.dumps(result)
+        return result
+
+class CategoriesCountry(Resource):
+
+    def post(self):
+        country = request.form['country']
+        dateBegin = request.form['dateBegin']
+        dateEnd = request.form['dateEnd']
+        analysis_type = request.form['analysis_type']
+        result = {"results": get_categories_trends_filtered_by_country(country, dateBegin, dateEnd, analysis_type)}
+        result = json.dumps(result)
+        return result
+
+
+class CartoDBTrends(Resource):
+
+    def post(self):
+        category = request.form['category']
+        dateBegin = request.form['dateBegin']
+        dateEnd = request.form['dateEnd']
+        analysis_type = request.form['analysis_type']
+        lst_country = get_countries_trends_filtered_by_category(category, dateBegin, dateEnd, analysis_type)
+        sync_db_view(category, lst_country)
+        return { "message": "ok" }
+
+
+api.add_resource(Status, '/status')
+api.add_resource(CountriesTrends, '/countriestrends')
+api.add_resource(CategoriesCountry, '/categoriestrends')
+api.add_resource(CartoDBTrends, '/cartodbview')
+
+if __name__ == '__main__':
+    print "Start Trend Api"
+    app.run(host='0.0.0.0', port=5001, debug=True)
