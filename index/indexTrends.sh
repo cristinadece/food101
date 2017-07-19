5302b2a505d61d80f5e61d6ec7fdbcab54a6c3e1
Cristina Muntean <cristina.i.muntean@gmail.com>
2017-07-19 20:19:53 +0200

    cris
    Former-commit-id: 1299bf98d31676c35e31d1e17eebe7bd8c4fdca2

--- index/indexTrends.sh
+++ index/indexTrends.sh
@@ -1,5 +1,5 @@
 for f in /home/foodmap/data/food-tweets/food-tweets-2017-03*;
 do
-  python index/index_trend.py -f $f -i trend
+  python index/index_trend.py -f $f -i trend -img /home/foodmap/data/img_classification/twitter.03.report.tsv
   sleep 1m
 done
