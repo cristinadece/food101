0e21ade29dbf514e097e2d745e298c1d7597281b
Vinícius Monteiro <vinicezarml@gmail.com>
2017-07-19 15:36:26 +0200

    not doing anymore refresh automatically, there is now a button for refresh
    Former-commit-id: 1a71bdb618bb50f1204aaf0927b69608ecf34469

--- containers/webapp/src/assets/js/streaming.js
+++ containers/webapp/src/assets/js/streaming.js
@@ -5,15 +5,24 @@
 var streamTime = null
 
 $(document).ready(function () {
-    streamTime = setInterval(refreshStreaming, 180 * 1000);
+    // streamTime = setInterval(refreshStreaming, 180 * 1000);
     refreshStreaming ();
+
+    $('#btn_refresh').click(function(){
+        refreshStreaming();
+    });
+
 });
 
 function refreshStreaming (){
+    $('#div_refresh').hide();
+
     $('#map').empty();
     var height_screen = $(window).height();
     $('#map').height(height_screen-60); // 60 heard height
     $('#map').append('<iframe width="100%" height="100%" frameborder="0" src="https://hpclab.carto.com/builder/0c64c066-609e-44ce-af6b-27edd1915df6/embed" allowfullscreen webkitallowfullscreen mozallowfullscreen oallowfullscreen msallowfullscreen></iframe>')
+
+    $('#div_refresh').fadeIn(400 * 12);
 }
 
 function stopRefresh(){
